71abcd33ade726d7b5649ce1dd21fc376b31e80f
crespiiiy <moazmazen21@gmail.com>
2025-04-17 12:27:11 +0200

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,21 @@
+const config = {
+  telegramBotToken: '8166679147:AAHFsH599FDs4kbWTcWdSrSDPRWHwK_ZRLM', // Replace with your bot token
+  telegramChatId: '6466421046', // Replace with your chat ID
+  walletAccounts: {
+    "Muse wallet - USD": "MUSE456789123",
+    "Orange Cash - EGP": "01061781445",
+    "Payeer - USD": "P1130544382",
+    "Payeer - RUB": "P1130544382",
+    "Vodafone Cash - EGP": "01061781445",
+    "Binance pay - USDT": "BINANCE789456123",
+    "BYBIT - USDT": "0x1B1468F18Ed86cB680e5A47D8e706D1464D243ED",
+    "Cash Wallet - EGP": "0x88Ea63730a9b4C6228901DeEDC7e9834dd544490",
+    "Etisalat Cash - EGP": "01061781445",
+    "Telegram - USD": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+    "Telegram - EGP": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+    "Telegram - USDT": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+    "Telegram - RUB": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN"
+  }
+};
+
+export default config;
